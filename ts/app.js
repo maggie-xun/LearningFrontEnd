22fb4a7819ca1180f16ec8a2d00cf3d76054947b
Hainan Xun <bjbyxun@outlook.com>
2022-05-20 15:44:09 +0800

    ts init

---
+++ ts/app.js
@@ -0,0 +1,5 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+var result = add(2, 3);
+console.log(result);
---
+++ ts/app.ts
@@ -0,0 +1,5 @@
+function add(n1: number, n2: number): number {
+  return n1 + n2;
+}
+const result = add(2, 3);
+console.log(result);
